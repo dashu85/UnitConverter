436d99d64fd52b2a55c7c5ad8cba55694fbc2e7e
Marcus Benoit <benoit.marcus@gmail.com>
2024-03-09 00:35:25 +0100

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Marcus Benoit on 09.03.24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
