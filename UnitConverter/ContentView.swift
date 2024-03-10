6fc03e216f904ded62ce8a9ed41a35e7a90d3820
Marcus Benoit <benoit.marcus@gmail.com>
2024-03-10 08:11:25 +0100

    UnitConverter, added Input textfield

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -5,20 +5,71 @@
 //  Created by Marcus Benoit on 09.03.24.
 //
 
+/*
+ Celsius, Fahrenheit or Kelvin
+ 
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var input: Double = 0
+    @State private var output: Double = 0
+    
+    enum Units: String, CaseIterable, Identifiable {
+        case Celsius, Fahrenheit, Kelvin
+        var id: Self { self }
+    }
+    
+    @State private var selectedInputUnit: Units = .Celsius
+    @State private var selectedOutputUnit: Units = .Fahrenheit
+
+    
+    @FocusState private var inputIsFocused: Bool
+    
+//    switch Convert {
+//    case CelsiusToFahrenheit:
+//        
+//    case CelsiusToKelvin:
+//        
+//    case FahrenheitToCelsius:
+//        
+//    case FahrenheitToKelvin:
+//        
+//    case KelvinToCelsius:
+//        
+//    case KelvinToFahrenheit:
+//        
+//    default:
+//}
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    HStack{
+                        TextField("Input Unit", value: $input, format: .number)
+                        Picker("", selection: $selectedInputUnit) {
+                            ForEach(Units.allCases) { unit in
+                                Text(unit.rawValue.capitalized)
+                            }
+                        }
+                    }
+                }
+                
+                Section {
+                    
+                }
+            }
+            .navigationTitle("Unit Converter")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
